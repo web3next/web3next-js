aef0ee5efe522d30c0f53254e14e201e6c30c300
zigen <hrlclb@gmail.com>
2018-06-15 00:21:51 +0900

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true
+  },
+  "extends": [
+    "canonical",
+    "canonical/ava",
+    "canonical/flowtype",
+    "canonical/jest"
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "promise"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "prefer-const": [
+      "error"
+    ],
+    "id-length": [
+      "off"
+    ],
+    "sort-keys": [
+      "off"
+    ]
+  }
+};
